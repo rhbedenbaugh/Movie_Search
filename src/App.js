84f6c54e42dfd053832b7be4bb76314d5310b442
Robert Bedenbaugh <95264763+rhbedenbaugh@users.noreply.github.com>
2023-02-12 14:57:49 -0500

    http to https for secure API calls with netlify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=a7287d95&';
+const API_URL = 'https://www.omdbapi.com/?apikey=a7287d95&';
 
 function App () {
   const [searchTerm, setSearchTerm] = useState('');
